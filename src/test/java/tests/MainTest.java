95d4a44320de1a6340cc105a34a197fe9a9445fe
g.alekseev <gleb1alekseev@gmail.com>
2025-04-11 17:30:54 +0300

    fixes

--- src/test/java/tests/MainTest.java
+++ src/test/java/tests/MainTest.java
@@ -1,11 +1,6 @@
 package tests;
 
-import com.codeborne.selenide.Condition;
-import org.testng.Assert;
 import org.testng.annotations.Test;
-
-import static com.codeborne.selenide.Selenide.switchTo;
-import static pages.MainPage.ENTRY_CREATED_WITH_DESCRIPTION;
 
 public class MainTest extends BaseTest{
 
@@ -15,16 +10,16 @@ public class MainTest extends BaseTest{
         mainSteps.logoutStep();
     }
 
-    @Test(description = "login and create an entry with description", invocationCount = 1)
-    public void createEntryWithDescriptionTest(){
-        loginSteps.loginWithUserAndPasswordStep(LOGIN_USER, LOGIN_PASSWORD, LOGIN_URL);
-        mainSteps.createEntryWithDescriptionStep(MAIN_ENTRY_DESCRIPTION);
-    }
-
     @Test(description = "login, create and delete an entry with description", invocationCount = 1)
     public void createAndDeleteEntryWithDescriptionTest(){
         loginSteps.loginWithUserAndPasswordStep(LOGIN_USER, LOGIN_PASSWORD, LOGIN_URL);
         mainSteps.createAndDeleteEntryWithDescriptionStep(MAIN_ENTRY_DESCRIPTION);
+    }
+
+    @Test(description = "login and create an entry with description", invocationCount = 1)
+    public void createEntryWithDescriptionTest(){
+        loginSteps.loginWithUserAndPasswordStep(LOGIN_USER, LOGIN_PASSWORD, LOGIN_URL);
+        mainSteps.createEntryWithDescriptionStep(MAIN_ENTRY_DESCRIPTION);
     }
 
     @Test(description = "login, create and search an entry with description")
