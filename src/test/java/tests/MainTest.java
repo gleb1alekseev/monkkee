0034a95ce96562c4ff26b511f7b7d5832d9d3795
g.alekseev <gleb1alekseev@gmail.com>
2025-04-22 21:29:15 +0300

    fix jenkins

--- src/test/java/tests/MainTest.java
+++ src/test/java/tests/MainTest.java
@@ -4,6 +4,12 @@ import org.testng.annotations.Test;
 
 public class MainTest extends BaseTest {
 
+    @Test(description = "login, create and delete an entry with description")
+    public void createAndDeleteEntryWithDescriptionTest() {
+        loginSteps.checkLoginWithUserAndPasswordStep(USER, PASSWORD, LOGIN_URL);
+        mainSteps.createAndDeleteEntryCheckStep(MAIN_ENTRY_DESCRIPTION);
+    }
+
     @Test(description = "success login with valid values and logout")
     public void logoutTest() {
         loginSteps.checkLoginWithUserAndPasswordStep(USER, PASSWORD, LOGIN_URL);
@@ -20,11 +26,5 @@ public class MainTest extends BaseTest {
     public void createAndSearchEntryWithDescriptionTest() {
         loginSteps.checkLoginWithUserAndPasswordStep(USER, PASSWORD, LOGIN_URL);
         mainSteps.createAndSearchEntryCheckStep(MAIN_ENTRY_DESCRIPTION);
-    }
-
-    @Test(description = "login, create and delete an entry with description")
-    public void createAndDeleteEntryWithDescriptionTest() {
-        loginSteps.checkLoginWithUserAndPasswordStep(USER, PASSWORD, LOGIN_URL);
-        mainSteps.createAndDeleteEntryCheckStep(MAIN_ENTRY_DESCRIPTION);
     }
 }
