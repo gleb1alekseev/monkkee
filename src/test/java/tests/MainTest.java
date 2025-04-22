1dd0d8afa31044a3fb0422320d96e60d71e1b58a
g.alekseev <gleb1alekseev@gmail.com>
2025-04-22 21:15:16 +0300

    fix jenkins

--- src/test/java/tests/MainTest.java
+++ src/test/java/tests/MainTest.java
@@ -2,7 +2,7 @@ package tests;
 
 import org.testng.annotations.Test;
 
-public class MainTest extends BaseTest{
+public class MainTest extends BaseTest {
 
     @Test(description = "success login with valid values and logout")
     public void logoutTest() {
@@ -10,21 +10,21 @@ public class MainTest extends BaseTest{
         mainSteps.logoutCheckStep();
     }
 
-    @Test(description = "login, create and delete an entry with description", invocationCount = 1)
-    public void createAndDeleteEntryWithDescriptionTest(){
+    @Test(description = "login and create an entry with description", invocationCount = 1)
+    public void createEntryWithDescriptionTest() {
         loginSteps.checkLoginWithUserAndPasswordStep(USER, PASSWORD, LOGIN_URL);
-        mainSteps.createAndDeleteEntryCheckStep(MAIN_ENTRY_DESCRIPTION);
+        mainSteps.createEntryWithDescriptionCheckStep(MAIN_ENTRY_DESCRIPTION);
     }
 
     @Test(description = "login, create and search an entry with description")
-    public void createAndSearchEntryWithDescriptionTest(){
+    public void createAndSearchEntryWithDescriptionTest() {
         loginSteps.checkLoginWithUserAndPasswordStep(USER, PASSWORD, LOGIN_URL);
         mainSteps.createAndSearchEntryCheckStep(MAIN_ENTRY_DESCRIPTION);
     }
 
-    @Test(description = "login and create an entry with description", invocationCount = 1)
-    public void createEntryWithDescriptionTest(){
+    @Test(description = "login, create and delete an entry with description", invocationCount = 1)
+    public void createAndDeleteEntryWithDescriptionTest() {
         loginSteps.checkLoginWithUserAndPasswordStep(USER, PASSWORD, LOGIN_URL);
-        mainSteps.createEntryWithDescriptionCheckStep(MAIN_ENTRY_DESCRIPTION);
+        mainSteps.createAndDeleteEntryCheckStep(MAIN_ENTRY_DESCRIPTION);
     }
 }
